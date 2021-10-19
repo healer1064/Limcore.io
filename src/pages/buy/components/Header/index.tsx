16c9c2a0b6217fb6e44fcb83867b50d253aeb4c9
emolokova <yaroslav4ik@yandex.ru>
2021-10-19 16:17:36 +0200

    Feature: component Header for <BuyPage />

---
+++ src/pages/buy/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './style.module.scss'
+
+import header from '../../../../assets/images/header.png'
+
+export const Header = () => {
+  return (
+    <div className={Styles.header_container}>
+      <img className={Styles.header_picture} src={header} alt='Шапка страницы' />
+    </div>
+  )
+}
--- src/pages/buy/index.tsx
+++ src/pages/buy/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Styles from './style.module.scss'
 
+import { ButtonBig } from '../../ui-kit/ButtonBig'
+import { InputRadio } from '../../ui-kit/InputRadio'
+import { InputText } from '../../ui-kit/InputText'
+import { Label } from '../../ui-kit/Label'
+import { Header } from './components/Header'
+
 export const BuyPage = () => {
   return (
     <div className={Styles.buy_container}>
-      <h1>Страница для покупки токенов</h1>
+      <Header />
+      {/* <ButtonBig /> */}
+      {/* <InputRadio titleRadio='Test' /> */}
+      {/* <InputText placeholder='TestTest' /> */}
+      {/* <Label>Test</Label> */}
     </div>
   )
 }
