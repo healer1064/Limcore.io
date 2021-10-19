29de63fc59b46efc377432178db8c9762a253fb0
emolokova <yaroslav4ik@yandex.ru>
2021-10-19 17:40:49 +0200

    Feature: components CloseButton and Title for <BuyPage />

--- src/pages/buy/index.tsx
+++ src/pages/buy/index.tsx
@@ -6,11 +6,17 @@ import { InputRadio } from '../../ui-kit/InputRadio'
 import { InputText } from '../../ui-kit/InputText'
 import { Label } from '../../ui-kit/Label'
 import { Header } from './components/Header'
+import { CloseButton } from './components/CloseButton'
+import { Title } from './components/Title'
 
 export const BuyPage = () => {
   return (
     <div className={Styles.buy_container}>
       <Header />
+      <div className={Styles.buy_title}>
+        <Title />
+        <CloseButton />
+      </div>
       {/* <ButtonBig /> */}
       {/* <InputRadio titleRadio='Test' /> */}
       {/* <InputText placeholder='TestTest' /> */}
