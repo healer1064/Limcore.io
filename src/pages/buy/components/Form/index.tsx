dbfbe2cd98cd4542910fd015b870cd3de40c530e
emolokova <yaroslav4ik@yandex.ru>
2021-10-22 13:27:33 +0200

    Fix: linked Labels and Inputs in component Form, BUT! Span + changeButton disable

--- src/pages/buy/components/Form/index.tsx
+++ src/pages/buy/components/Form/index.tsx
@@ -17,23 +17,25 @@ export const Form = ({ title, content, promo, message }) => {
         <p className={Styles.form__paragraph_promo}>{promo}</p>
         <p className={Styles.form__paragraph}>{content}</p>
         <fieldset className={Styles.form__fieldset}>
-          <Label titleText='Сумма покупки' />
-          <InputText placeholder='Введите сумму в $' className={Styles.form__input} />
+          <Label titleText='Сумма покупки'>
+            <InputText placeholder='Введите сумму в $' className={Styles.form__input} />
+          </Label>
           <button className={Styles.form__switchIcon} type='button' />
-          <Label titleText='Количество токенов' />
-          <InputText placeholder='Введите количество токенов' className={Styles.form__input} />
+          <Label titleText='Количество токенов'>
+            <InputText placeholder='Введите количество токенов' className={Styles.form__input} />
+          </Label>
         </fieldset>
         <h3 className={Styles.form__subtitle}>Введите телефон или email</h3>
         <fieldset className={Styles.form__fieldset}>
           <Label titleText={'Телефон' || 'Мы отправили код на номер'} className={Styles.form__label}>
-            <div className={Styles.form__labelContainer}>
-              <span className={Styles.form__notification_invisible}>+7 (999) 098 65−34</span>
-              <button className={Styles.form__changeButton_invisible} type='button'>
-                Изменить
-              </button>
-            </div>
+            {/* <div className={Styles.form__labelContainer}> */}
+            {/*  <span className={Styles.form__notification_invisible}>+7 (999) 098 65−34</span> */}
+            {/*  <button className={Styles.form__changeButton_invisible} type='button'> */}
+            {/*    Изменить */}
+            {/*  </button> */}
+            {/* </div> */}
+            <InputText placeholder='Введите телефон' className={Styles.form__input} />
           </Label>
-          <InputText placeholder='Введите телефон' className={Styles.form__input} />
           <span className={Styles.form__notification_invisible}>{message}</span>
           <button className={Styles.form__changeButton_invisible} type='button'>
             Отправить новый код
@@ -41,8 +43,9 @@ export const Form = ({ title, content, promo, message }) => {
           <ButtonForCode>Получить код</ButtonForCode>
         </fieldset>
         <fieldset className={Styles.form__fieldset}>
-          <Label titleText='Email' />
-          <InputText placeholder='Введите e-mail' className={Styles.form__input} />
+          <Label titleText='Email'>
+            <InputText placeholder='Введите e-mail' className={Styles.form__input} />
+          </Label>
         </fieldset>
         <h3 className={Styles.form__subtitle}>Выберите способ оплаты</h3>
         <div className={Styles.form__cardsContainer}>
--- src/pages/buy/index.tsx
+++ src/pages/buy/index.tsx
@@ -1,32 +1,27 @@
 import React from 'react'
 import Styles from './style.module.scss'
 
-import { Header } from './components/Header'
-import { CloseButton } from './components/CloseButton'
-import { Title } from './components/Title'
 import { Form } from './components/Form'
 import { TermsOfUse } from './components/TermsOfUse'
+import { Container } from '@components/Container'
 
 export const BuyPage = () => {
   return (
     <div className={Styles.buy}>
-      <Header />
-      <div className={Styles.buy__title}>
-        <Title />
-        <CloseButton />
-      </div>
-      <Form
-        title='Покупка LIMC'
-        content='Вы покупаете 1 Limcore Token по цене 200$ с Lock-up периодом 6 месяцев'
-        promo='Промокод применен'
-        message={
-          'Получить новый код можно через 00:41' ||
-          'Неверный формат телефона' ||
-          'Неверный код. Попробуйте еще раз' ||
-          'Вы забыли ввести номер телефона'
-        }
-      />
-      <TermsOfUse />
+      <Container title='Покупка'>
+        <Form
+          title='Покупка LIMC'
+          content='Вы покупаете 1 Limcore Token по цене 200$ с Lock-up периодом 6 месяцев'
+          promo='Промокод применен'
+          message={
+            'Получить новый код можно через 00:41' ||
+            'Неверный формат телефона' ||
+            'Неверный код. Попробуйте еще раз' ||
+            'Вы забыли ввести номер телефона'
+          }
+        />
+        <TermsOfUse />
+      </Container>
     </div>
   )
 }
