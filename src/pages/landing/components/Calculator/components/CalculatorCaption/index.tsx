99ee2a55f0e540ce98f77e00bb87b43bbd606275
opv1 <pas7273@gmail.com>
2021-10-24 14:30:30 +0300

    edit calc

--- src/pages/landing/components/Calculator/components/CalculatorCaption/index.tsx
+++ src/pages/landing/components/Calculator/components/CalculatorCaption/index.tsx
@@ -24,17 +24,47 @@ export const CalculatorCaption: React.FC = () => {
       <span className={Styles.designation}>Все данные актуальны на текущий момент</span>
       <form className={Styles.form}>
         <div className={Styles.block}>
-          <Label titleText='Выберите количество LIMC'>
+          <Label className={Styles.label} titleText='Выберите количество LIMC'>
             <InputText value='40,000 LIMC' placeholder='' />
           </Label>
-          <Label titleText='Сумма инвестиций в USDT'>
+          <Label className={Styles.label} titleText='Сумма инвестиций в USDT'>
             <InputText value='3,800,000 USDT' placeholder='' />
           </Label>
         </div>
         <div className={Styles.block}>
           <div className={Styles.info}>
-            <div className={Styles.wrap}>
+            <div className={Styles.container}>
               <span className={Styles.signification}>Распределение вознаграждений</span>
+              <div className={Styles.line}>
+                <div className={Styles.column}>
+                  <strong>Холдер LIMC</strong>
+                  <strong>85%</strong>
+                </div>
+                <div className={Styles.column}>
+                  <strong>Limcore</strong>
+                  <strong>15%</strong>
+                </div>
+              </div>
+              <div className={Styles.wrap}>
+                <div className={Styles.inner}>
+                  <span>Доход с 85%</span>
+                  <span>21,6% годовых в $</span>
+                </div>
+                <div className={Styles.row}>
+                  <div className={Styles.inner}>
+                    <span>В час</span>
+                    <span>$93,6</span>
+                  </div>
+                  <div className={Styles.inner}>
+                    <span>В день</span>
+                    <span>$2,248.7</span>
+                  </div>
+                  <div className={Styles.inner}>
+                    <span>В месяц</span>
+                    <span>$68,400</span>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
           <ButtonBig>Купить LIMC</ButtonBig>
