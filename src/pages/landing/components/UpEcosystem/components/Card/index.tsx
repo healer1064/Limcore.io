71f62f23c1a7588b97ef8776937a77e5b100e26e
Kristina Gromova <christina28romova@yandex.ru>
2021-10-25 09:59:25 +0300

    components upecosystem,online

---
+++ src/pages/landing/components/UpEcosystem/components/Card/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Styles from './styles.module.scss'
+import mark from '@icons/limcore.svg'
+import arrowUp from '@icons/arrow-up-green.svg'
+import arrowDown from '@icons/arrow-down-red.svg'
+
+export const Card = (props) => {
+  return (
+    <button
+      className={`${Styles.container} ${props.isActive && Styles.container_active}`}
+      id={props.item.id}
+      onClick={props.onClick}
+    >
+      <h6 className={Styles.title}>{props.item.title}</h6>
+      {props.item.isMark && (
+        <div className={Styles.mark__container}>
+          <img src={mark} alt='mark' />
+          <p className={Styles.mark__text}>{props.item.text}</p>
+        </div>
+      )}
+      <div className={Styles.value}>{props.item.value}</div>
+      <div className={Styles.dinamic__container}>
+        <div className={Styles.dinamic__container}>
+          {props.item.isUp ? (
+            <>
+              <img src={arrowUp} alt='up' />
+              <span className={`${Styles.dinamic} ${Styles.dinamic__up}`}>{props.item.dinamikValue}%</span>
+            </>
+          ) : (
+            <>
+              <img src={arrowDown} alt='down' />
+              <span className={`${Styles.dinamic} ${Styles.dinamic__down}`}>{props.item.dinamikValue}%</span>
+            </>
+          )}{' '}
+          <span className={Styles.dinamic}>last 30 days</span>
+        </div>
+      </div>
+    </button>
+  )
+}
--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -3,12 +3,15 @@ import Styles from './styles.module.scss'
 
 import { Main } from './components/Main'
 import { Calculator } from './components/Calculator'
-
+import { UpEcosystem } from './components/UpEcosystem'
+import { OnLine } from './components/OnLine'
 export const LandingPage: React.FC = () => {
   return (
     <div className={Styles.landing}>
       <Main />
       <Calculator />
+      <UpEcosystem />
+      <OnLine />
     </div>
   )
 }
