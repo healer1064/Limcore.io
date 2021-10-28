6e0770621ba0a2676065b809de8854d497476d72
opv1 <pas7273@gmail.com>
2021-10-28 12:53:09 +0300

    more beauty

--- src/pages/landing/components/Questions/QuestionsMobile/index.tsx
+++ src/pages/landing/components/Questions/QuestionsMobile/index.tsx
@@ -1,92 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './styles.module.scss'
 
 import { VectorIcon } from '@icons/VectorIcon'
 
 export const QuestionsMobile: React.FC = () => {
+  const array = [
+    { id: 1, title: 'С чего начать?', content: '' },
+    { id: 2, title: 'Как купить LIMC?', content: '' },
+    { id: 3, title: 'Как зарегистрироваться на limcore.io?', content: '' },
+    { id: 4, title: 'С чего начать?', content: '' },
+    { id: 5, title: 'Как купить LIMC?', content: '' },
+    { id: 6, title: 'Как зарегистрироваться на limcore.io?', content: '' },
+    { id: 7, title: 'С чего начать?', content: '' },
+  ]
+
+  const [activeId, setActiveId] = useState(null)
+
+  const setActiveItem = (id) => {
+    if (id === activeId) {
+      setActiveId(null)
+    } else {
+      setActiveId(id)
+    }
+  }
+
   return (
     <>
       <div className={Styles.wrapper}>
         <h4 className={Styles.caption}>Вопросы и ответы</h4>
         <ul className={Styles.question}>
-          <li className={Styles.item}>
-            <div className={Styles.row}>
-              <span>С чего начать?</span>
-              <VectorIcon />
-            </div>
-          </li>
-          <li className={Styles.item}>
-            <div className={Styles.row}>
-              <span>Как купить LIMC?</span>
-              <VectorIcon />
-            </div>
-          </li>
-          <li className={Styles.item}>
-            <div className={Styles.row}>
-              <span>Как зарегистрироваться на limcore.io?</span>
-              <VectorIcon />
-            </div>
-          </li>
-          <li className={`${Styles.item} ${Styles.item_active}`}>
-            <div className={Styles.row}>
-              <span>С чего начать?</span>
-              <VectorIcon />
-            </div>
-            <div className={Styles.block}>
-              <p className={Styles.text}>
-                Иногда после верификации компания принимает решение о дополнительной проверке биографии клиента. Ее цель
-                – оценка риска.
-              </p>
-              <span className={Styles.description}>Как устроена стандартная процедура KYC?</span>
-              <p className={Styles.text}>
-                Для получения полного доступа и увеличенного лимита ввода и вывода средств пользователям необходимо
-                завершить верификацию.
-              </p>
-              <ul className={Styles.list}>
-                <li>
-                  <p className={Styles.text}>
-                    Подобно другим финансовым институтам, ведущие криптовалютные биржи во всем мире требуют от клиентов
-                    пройти обязательную верификацию для получения постоянного доступа к услугам
-                  </p>
+          {array &&
+            array.map((item) => {
+              return (
+                <li
+                  key={item.id}
+                  className={activeId === item.id ? `${Styles.item} ${Styles.item_active}` : `${Styles.item}`}
+                >
+                  <div className={Styles.row} onClick={() => setActiveItem(item.id)}>
+                    <span>{item.title}</span>
+                    <VectorIcon />
+                  </div>
+                  <div className={Styles.block}>
+                    <p className={Styles.text}>
+                      Иногда после верификации компания принимает решение о дополнительной проверке биографии клиента.
+                      Ее цель – оценка риска.
+                    </p>
+                    <span className={Styles.description}>Как устроена стандартная процедура KYC?</span>
+                    <p className={Styles.text}>
+                      Для получения полного доступа и увеличенного лимита ввода и вывода средств пользователям
+                      необходимо завершить верификацию.
+                    </p>
+                    <ul className={Styles.list}>
+                      <li>
+                        <p className={Styles.text}>
+                          Подобно другим финансовым институтам, ведущие криптовалютные биржи во всем мире требуют от
+                          клиентов пройти обязательную верификацию для получения постоянного доступа к услугам
+                        </p>
+                      </li>
+                      <li>
+                        <p className={Styles.text}>
+                          Подобно другим финансовым институтам, ведущие криптовалютные биржи во всем мире требуют от
+                          клиентов пройти обязательную верификацию для получения постоянного доступа к услугам
+                        </p>
+                      </li>
+                      <li>
+                        <p className={Styles.text}>
+                          Подобно другим финансовым институтам, ведущие криптовалютные биржи во всем мире требуют от
+                          клиентов пройти обязательную верификацию для получения постоянного доступа к услугам
+                        </p>
+                      </li>
+                    </ul>
+                    <p className={Styles.text}>
+                      Процедуры верификации могут отличаться в зависимости от характера бизнеса, однако преследуют они
+                      одни и те же цели. Основные этапы процедуры – сбор и проверка данных. Сюда также относится
+                      комплексная проверка и постоянный мониторинг пользователей.
+                    </p>
+                  </div>
                 </li>
-                <li>
-                  <p className={Styles.text}>
-                    Подобно другим финансовым институтам, ведущие криптовалютные биржи во всем мире требуют от клиентов
-                    пройти обязательную верификацию для получения постоянного доступа к услугам
-                  </p>
-                </li>
-                <li>
-                  <p className={Styles.text}>
-                    Подобно другим финансовым институтам, ведущие криптовалютные биржи во всем мире требуют от клиентов
-                    пройти обязательную верификацию для получения постоянного доступа к услугам
-                  </p>
-                </li>
-              </ul>
-              <p className={Styles.text}>
-                Процедуры верификации могут отличаться в зависимости от характера бизнеса, однако преследуют они одни и
-                те же цели. Основные этапы процедуры – сбор и проверка данных. Сюда также относится комплексная проверка
-                и постоянный мониторинг пользователей.
-              </p>
-            </div>
-          </li>
-          <li className={Styles.item}>
-            <div className={Styles.row}>
-              <span>Как купить LIMC?</span>
-              <VectorIcon />
-            </div>
-          </li>
-          <li className={Styles.item}>
-            <div className={Styles.row}>
-              <span>Как зарегистрироваться на limcore.io?</span>
-              <VectorIcon />
-            </div>
-          </li>
-          <li className={Styles.item}>
-            <div className={Styles.row}>
-              <span>С чего начать?</span>
-              <VectorIcon />
-            </div>
-          </li>
+              )
+            })}
         </ul>
       </div>
     </>
--- src/pages/landing/components/Questions/index.tsx
+++ src/pages/landing/components/Questions/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './styles.module.scss'
 
 import { QuestionsMobile } from './QuestionsMobile'
@@ -6,6 +6,22 @@ import { QuestionsMobile } from './QuestionsMobile'
 import { VectorIcon } from '@icons/VectorIcon'
 
 export const Questions: React.FC = () => {
+  const array = [
+    { id: 1, title: 'С чего начать?', content: '' },
+    { id: 2, title: 'Как купить LIMC?', content: '' },
+    { id: 3, title: 'Как зарегистрироваться на limcore.io?', content: '' },
+    { id: 4, title: 'С чего начать?', content: '' },
+    { id: 5, title: 'Как купить LIMC?', content: '' },
+    { id: 6, title: 'Как зарегистрироваться на limcore.io?', content: '' },
+    { id: 7, title: 'С чего начать?', content: '' },
+  ]
+
+  const [activeItem, setActiveItem] = useState({ id: null, title: '', content: '' })
+
+  useEffect(() => {
+    setActiveItem(array[0])
+  }, [])
+
   return (
     <>
       <div className={`${Styles.questions} ${Styles.questions_desktop}`}>
@@ -13,37 +29,20 @@ export const Questions: React.FC = () => {
           <h4 className={Styles.caption}>Вопросы и ответы</h4>
           <div className={Styles.container}>
             <ul className={Styles.question}>
-              <li className={`${Styles.item} ${Styles.item_active}`}>
-                <span>С чего начать?</span>
-                <VectorIcon />
-              </li>
-              <li className={Styles.item}>
-                <span>Как купить LIMC?</span>
-                <VectorIcon />
-              </li>
-              <li className={Styles.item}>
-                <span>Как зарегистрироваться на limcore.io?</span>
-                <VectorIcon />
-              </li>
-              <li className={Styles.item}>
-                <span>С чего начать?</span>
-                <VectorIcon />
-              </li>
-              <li className={Styles.item}>
-                <span>Как купить LIMC?</span>
-                <VectorIcon />
-              </li>
-              <li className={Styles.item}>
-                <span>Как зарегистрироваться на limcore.io?</span>
-                <VectorIcon />
-              </li>
-              <li className={Styles.item}>
-                <span>С чего начать?</span>
-                <VectorIcon />
-              </li>
+              {array &&
+                array.map((item) => (
+                  <li
+                    key={item.id}
+                    className={activeItem.id === item.id ? `${Styles.item} ${Styles.item_active}` : `${Styles.item}`}
+                    onClick={() => setActiveItem(item)}
+                  >
+                    <span>{item.title}</span>
+                    <VectorIcon />
+                  </li>
+                ))}
             </ul>
             <div className={Styles.info}>
-              <h5 className={Styles.title}>С чего начать?</h5>
+              <h5 className={Styles.title}>{activeItem.title}</h5>
               <p className={Styles.text}>
                 Иногда после верификации компания принимает решение о дополнительной проверке биографии клиента. Ее цель
                 – оценка риска.
