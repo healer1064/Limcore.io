67064d8cb2aa38f91aca973fbff0cf2e74cdc648
healer1064729 <healer1064729@gmail.com>
2021-11-01 09:33:34 +0300

    feature/reg-new-text+purse-number

--- src/components/Purse/PurseMobile/components/Balance/index.tsx
+++ src/components/Purse/PurseMobile/components/Balance/index.tsx
@@ -1,15 +1,25 @@
 import React, { useState } from 'react'
 import styles from './styles.module.scss'
-import { balance as balanceSvg } from '../../images'
+import { balance as balanceSvg, copyIcon } from '../../images'
+import logoIcon from '@icons/logo.svg'
 import { Modal } from '../Modal'
 import { Overall } from './components/Overall/index'
 import { useAppSelector } from '@app/redux/hooks'
+import { ButtonBig } from '../../../../../ui-kit/ButtonBig'
 
 export const Balance = () => {
   const [isBalanceVisible, setIsBalanceVisible] = useState(false)
+  // Если человек попал в личныый кабинет через регистрацию, то тут будет true
+  const [isRegModalVisible, setIsRegModalVisible] = useState(
+    useAppSelector((state) => state.auth.processType) === 'REGISTRATION',
+  )
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [money, setMoney] = useState('0')
   const walletAddress = useAppSelector((state) => state.wallet.address)
+
+  const handleFirstRegModalClose = () => {
+    setIsRegModalVisible(false)
+  }
 
   const handleOpenBalanceClick = () => {
     setIsBalanceVisible(true)
@@ -41,6 +51,27 @@ export const Balance = () => {
       </div>
       <Modal active={isBalanceVisible} setActive={() => {}}>
         <Overall onClick={handleCloseBalanceModal} money={money} />
+      </Modal>
+      <Modal classname={styles.reg} active={isRegModalVisible} setActive={handleFirstRegModalClose} crossFlag>
+        <div className={styles.regModal}>
+          <div className={styles.regModalUp}>
+            <header className={styles.regModalHeader}>
+              <img src={logoIcon} onClick={handleFirstRegModalClose} />
+            </header>
+            <h4 className={styles.regModalTitle}>Мы создали ваш USDT кошелек</h4>
+            <p className={styles.regModalSubtitle}>Адрес кошелька</p>
+            <p className={styles.regModalPurse}>
+              qp8r78kv2rrctgsdmknvvmt0rjs8szjcmvcsge5pz8
+              <img className={styles.regModalPurseCopy} src={copyIcon} />
+            </p>
+          </div>
+          <div className={styles.regModalDown}>
+            <ButtonBig className={styles.regModalButton} onClick={handleFirstRegModalClose}>
+              Пополнить кошелек
+            </ButtonBig>
+            <p className={styles.regModalSubtitle}>На данный момент LIMC можно купить только с помощью USDT ERC-20</p>
+          </div>
+        </div>
       </Modal>
     </div>
   )
--- src/pages/auth/components/Auth/parts/Form/Form.tsx
+++ src/pages/auth/components/Auth/parts/Form/Form.tsx
@@ -26,10 +26,6 @@ import { getValidationSchema } from '../../helpers/yup.helpers'
 import { isNumbersOnly } from '../../helpers/number.helpers'
 import styles from './Form.module.scss'
 import useWindowSize from '@helpers/useWindowSizeHook'
-import { Modal } from '@components/Purse/PurseMobile/components/Modal'
-import logoIcon from '@icons/logo.svg'
-import { ButtonBig } from '../../../../../../ui-kit/ButtonBig'
-import { copyIcon } from '@components/Purse/PurseMobile/images'
 
 const Form: FC = () => {
   const history = useHistory()
@@ -38,7 +34,6 @@ const Form: FC = () => {
   const isLimcClick = useAppSelector(authSelector).isBuyLimcClick
   const [timeLeft, { start }] = useCountDown(initialTime, interval)
   const [opts, setOpts] = useState<any>({})
-  const [isFirstRegModalVisible, setIsFirstRegModalVisible] = useState(false)
   const { ref, maskRef } = useIMask(opts)
 
   const [phone, setPhone] = useLocalStorage('phone', '')
@@ -78,17 +73,8 @@ const Form: FC = () => {
       code: numberCode,
       unique_identifier: uniqueId,
     }
-    dispatch(registerUserEmailConfirmation(data)).then(() => setIsFirstRegModalVisible(true))
-    // setIsFirstRegModalVisible(true)
-    // dispatch(getJwtToken({ email, code: data.code }))
-    // history.push('/')
-  }
-
-  // const handleFirstRegModalOpen = () => {
-  //   setIsFirstRegModalVisible(true)
-  // }
-  const handleFirstRegModalClose = () => {
-    setIsFirstRegModalVisible(false)
+    dispatch(registerUserEmailConfirmation(data))
+    dispatch(getJwtToken({ email, code: data.code }))
     history.push('/')
   }
 
@@ -525,36 +511,6 @@ const Form: FC = () => {
                       Зарегистрироваться
                     </Button>
                   )}
-
-                  <Modal
-                    classname={styles.reg}
-                    active={isFirstRegModalVisible}
-                    setActive={handleFirstRegModalClose}
-                    crossFlag
-                  >
-                    <div className={styles.regModal}>
-                      {console.log(`isFirstRegModalVisible: ${isFirstRegModalVisible}`)}
-                      <div className={styles.regModalUp}>
-                        <header className={styles.regModalHeader}>
-                          <img src={logoIcon} onClick={() => history.push('/')} />
-                        </header>
-                        <h4 className={styles.regModalTitle}>Мы создали ваш USDT кошелек</h4>
-                        <p className={styles.regModalSubtitle}>Адрес кошелька</p>
-                        <p className={styles.regModalPurse}>
-                          qp8r78kv2rrctgsdmknvvmt0rjs8szjcmvcsge5pz8
-                          <img className={styles.regModalPurseCopy} src={copyIcon} />
-                        </p>
-                      </div>
-                      <div className={styles.regModalDown}>
-                        <ButtonBig className={styles.regModalButton} onClick={() => history.push('/')}>
-                          Пополнить кошелек
-                        </ButtonBig>
-                        <p className={styles.regModalSubtitle}>
-                          На данный момент LIMC можно купить только с помощью USDT ERC-20
-                        </p>
-                      </div>
-                    </div>
-                  </Modal>
                 </FormikForm>
               )}
             </Formik>
