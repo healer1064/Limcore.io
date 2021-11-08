ad3d105732cf54509b41a444d81419667f362311
Andrey Tsivtsivadze <andrey.zonter@yandex.ru>
2021-11-08 20:43:27 +0300

    some fixies broadcast component

--- src/components/Broadcasts/BroadcastsMobile/index.tsx
+++ src/components/Broadcasts/BroadcastsMobile/index.tsx
@@ -1,52 +1,40 @@
 import React from 'react'
 import styles from './styles.module.scss'
-import icon from '@icons/VideoCamera.svg'
+import { BroadcastItem } from './components/broadcastItem'
+
+const broadcasts = [
+  {
+    number: 1,
+    src: 'https://rtsp.me/embed/iRTG98h5/',
+    title: 'Ремонт здания',
+  },
+  {
+    number: 2,
+    src: 'https://rtsp.me/embed/TrD56fbb/',
+    title: 'Ремонт здания',
+  },
+  {
+    number: 3,
+    src: 'https://rtsp.me/embed/bK2f3neH/',
+    title: 'Ремонт прилегающей территории',
+  },
+  {
+    number: 4,
+    src: 'https://rtsp.me/embed/7rDYNfA5/',
+    title: 'Ремонт прилегающей территории',
+  },
+]
 
 export const BroadcastsMobile = () => {
   return (
     <div className={styles.broadcast}>
-      <div className={styles.broadcast__header}>
-        <h2>On-line</h2>
-        <img src={icon} alt='Camera' />
-      </div>
-      <div className={styles.broadcast__items}>
-        <div className={styles.broadcast__wrapper}>
-          <div className={styles.broadcast__item}>
-            <iframe
-              className={styles.broadcast__videoFrame}
-              src='https://rtsp.me/embed/iRTG98h5/'
-              frameBorder='0'
-              allowFullScreen
-            />
-          </div>
-          <div className={styles.broadcast__item}>
-            <iframe
-              className={styles.broadcast__videoFrame}
-              src='https://rtsp.me/embed/TrD56fbb/'
-              frameBorder='0'
-              allowFullScreen
-            />
-          </div>
-        </div>
-        <div className={styles.broadcast__wrapper}>
-          <div className={styles.broadcast__item}>
-            <iframe
-              className={styles.broadcast__videoFrame}
-              src='https://rtsp.me/embed/bK2f3neH/'
-              frameBorder='0'
-              allowFullScreen
-            />
-          </div>
-          <div className={styles.broadcast__item}>
-            <iframe
-              className={styles.broadcast__videoFrame}
-              src='https://rtsp.me/embed/7rDYNfA5/'
-              frameBorder='0'
-              allowFullScreen
-            />
-          </div>
-        </div>
-      </div>
+      <ul className={styles.broadcast__items}>
+        {broadcasts.map((broadcast) => (
+          <li className={styles.broadcast__item_wrapper} key={broadcast.number}>
+            <BroadcastItem broadcast={broadcast} />
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
