50ec30bda0a32b9c898ec9d2dd12f40602e103ef
emolokova <yaroslav4ik@yandex.ru>
2021-11-10 13:25:23 +0100

    Feature: unfinished slider

--- src/components/Purse/PurseDesktop/components/Wallet/index.tsx
+++ src/components/Purse/PurseDesktop/components/Wallet/index.tsx
@@ -1,45 +1,52 @@
 import React, { FC, useRef, useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import styles from './styles.module.scss'
 
 import { walletSvg } from '../../images'
-import GrayCrossIcon from '../../images/GrayCross/GrayCrossIcon'
 import { ArrowRight } from '@icons/ArrowRight'
 import { ArrowLeft } from '@icons/ArrowLeft'
+import 'swiper/swiper.scss'
+import 'swiper/components/navigation/navigation.scss'
+import 'swiper/components/pagination/pagination.scss'
+import 'swiper/components/scrollbar/scrollbar.scss'
+import SwiperCore, { Pagination } from 'swiper'
+SwiperCore.use([Pagination])
 
 // type WalletPropsType = {}
 
 export const Wallet = () => {
-  // const roadmapRef = useRef<HTMLDivElement>(null)
-  const [isArrowHidden, setIsArrowHidden] = useState(true)
-  // const scrollLeft = () => {
-  //   roadmapRef.current.scrollLeft += 300
-  //   if (roadmapRef.current.scrollLeft !== 0) {
-  //     setIsArrowHidden(false)
-  //   }
-  // }
-  // const scrollRight = () => {
-  //   roadmapRef.current.scrollLeft -= 300
-  //   if (roadmapRef.current.scrollLeft === 0) {
-  //     setIsArrowHidden(true)
-  //   }
-  // }
+  const params = {
+    slidesPerView: 1,
+  }
+
   return (
-    <div className={styles.wallet}>
-      <p className={styles.wallet__title}>Привяжите внешние кошельки</p>
-      <p className={styles.wallet__subtitle}>Добавляйте свои внешние кошельки к форкам </p>
-      <img src={walletSvg} className={styles.wallet__image} />
-      <button className={styles.moreButton} type='button'>
-        Подробнее
-      </button>
+    <Swiper className={styles.swiper} pagination {...params} spaceBetween={10}>
       <button className={`${styles.scrollButton} ${styles.scrollButtonLeft}`} type='button'>
         <ArrowRight className={styles.scrollIcon} />
       </button>
-      <button
-        className={`${styles.scrollButton} ${styles.scrollButtonRight} ${isArrowHidden && styles.arrowHidden}`}
-        type='button'
-      >
+      <button className={`${styles.scrollButton} ${styles.scrollButtonRight}`} type='button'>
         <ArrowLeft className={styles.scrollIcon} />
       </button>
-    </div>
+      <SwiperSlide className={styles.slide}>
+        <div className={styles.wallet}>
+          <p className={styles.wallet__title}>Привяжите внешние кошельки</p>
+          <p className={styles.wallet__subtitle}>Добавляйте свои внешние кошельки к форкам </p>
+          <img src={walletSvg} className={styles.wallet__image} />
+          <button className={styles.moreButton} type='button'>
+            Подробнее
+          </button>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className={styles.slide}>
+        <div className={styles.wallet}>
+          <p className={styles.wallet__title}>Привяжите внешние кошельки</p>
+          <p className={styles.wallet__subtitle}>Добавляйте свои внешние кошельки к форкам </p>
+          <img src={walletSvg} className={styles.wallet__image} />
+          <button className={styles.moreButton} type='button'>
+            Подробнее
+          </button>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   )
 }
