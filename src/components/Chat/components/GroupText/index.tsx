71331ee2cd9c1a57bdbd428864f9910271be4dea
emolokova <yaroslav4ik@yandex.ru>
2021-11-23 16:11:02 +0100

    Feature/Group component scss

--- src/components/Chat/components/GroupText/index.tsx
+++ src/components/Chat/components/GroupText/index.tsx
@@ -4,10 +4,12 @@ import styles from './styles.module.scss'
 export const GroupText = ({ member }) => {
   return (
     <div className={styles.member}>
-      <img src={member.foto} />
-      <img src={member.raitingIcon} />
-      <span>{member.score}</span>
-      <p>{member.message}</p>
+      <img src={member.foto} alt='' className={styles.foto} />
+      <span className={styles.raiting}>
+        <img src={member.raitingIcon} alt='' className={styles.raitingIcon} />
+        <span>{`${member.score} ТВ`}</span>
+      </span>
+      <p className={styles.message}>{member.message}</p>
     </div>
   )
 }
