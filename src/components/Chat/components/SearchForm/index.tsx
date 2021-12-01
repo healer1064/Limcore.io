2065a0d4e4d70667ff4fa670bcd8da3c1a3f65da
emolokova <yaroslav4ik@yandex.ru>
2021-12-01 12:53:56 +0100

    Improvement/refactoring components

--- src/components/Chat/components/SearchForm/index.tsx
+++ src/components/Chat/components/SearchForm/index.tsx
@@ -26,9 +26,9 @@ export const SearchForm = ({ desktop }) => {
     dispatch(setIsSearched(''))
   }
 
-  return desktop ? (
+  return (
     <>
-      <form className={styles.searchForm_desktop}>
+      <form className={desktop ? styles.searchForm_desktop : styles.searchForm}>
         <input
           type='search'
           className={styles.searchInput}
@@ -47,34 +47,6 @@ export const SearchForm = ({ desktop }) => {
           {t('chat_reset_button_value')}
         </button>
       </form>
-      <p className={`${styles.subtitle} ${styles.subtitle_invisible}`}>{t('chat_no_results')}</p>
-      <p className={`${styles.text} ${styles.text_invisible}`}>{`${t('chat_no_results_text')} '${searched}'`}</p>
-      <p className={`${styles.text} ${styles.text_invisible}`}>{t('chat_no_results_try')}</p>
-    </>
-  ) : (
-    <>
-      <form className={styles.searchForm}>
-        <input
-          type='search'
-          className={styles.searchInput}
-          placeholder={t('chat_form_placeholder')}
-          onChange={(e) => handleChange(e)}
-          onFocus={handleResetButton}
-          value={searched}
-        />
-        <img
-          className={closeButtonVisible ? styles.closeButton : styles.closeButton_invisible}
-          alt=''
-          src={closeButton}
-          onClick={handleCloseSearch}
-        />
-        <button type='reset' className={resetButtonVisible ? styles.resetButton : styles.resetButton_invisible}>
-          {t('chat_reset_button_value')}
-        </button>
-      </form>
-      <p className={`${styles.subtitle} ${styles.subtitle_invisible}`}>{t('chat_no_results')}</p>
-      <p className={`${styles.text} ${styles.text_invisible}`}>{`${t('chat_no_results_text')} '${searched}'`}</p>
-      <p className={`${styles.text} ${styles.text_invisible}`}>{t('chat_no_results_try')}</p>
     </>
   )
 }
