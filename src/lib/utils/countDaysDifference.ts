b3fba2dddbbb52ef84804a10c19bd3dd29ff7bb0
Andrey Tsivtsivadze <andrey.zonter@yandex.ru>
2022-01-20 18:43:54 +0300

    fx safari time

--- src/lib/utils/countDaysDifference.ts
+++ src/lib/utils/countDaysDifference.ts
@@ -2,7 +2,7 @@
 import differenceInCalendarDays from 'date-fns/differenceInCalendarDays'
 
 export const countDaysDifference = () => {
-  let difference = differenceInCalendarDays(new Date('2022, 3, 21'), new Date())
+  let difference = differenceInCalendarDays(1647810000000, new Date())
   difference > 80 ? (difference = 80) : difference
   return difference < 0 ? 0 : difference
 }
