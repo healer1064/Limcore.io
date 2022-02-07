311e329a59bbbba4575d8a5de4a6f79d2eca8e36
Avenant <avenantmar@gmail.com>
2022-02-07 22:11:38 +0300

    update GamificationOfMining block

--- src/pages/landing/components/GamificationOfMining/index.tsx
+++ src/pages/landing/components/GamificationOfMining/index.tsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import style from './styles.module.scss'
 import { GameCaption } from './components/GameCaption'
 import { Garden } from './components/Garden'
 import { GameStart } from './components/GameStart'
+import { Blockquote } from './components/GameStart/Blockquote'
 
 export const GamificationOfMining: React.FC = () => {
+  const { innerWidth: width } = window
+
+  const [clientWidth, setClientWidth] = useState<number>(width)
+
+  useEffect(() => {
+    const resizeHandler = () => setClientWidth(window.innerWidth)
+    window.addEventListener('resize', resizeHandler)
+    return () => window.removeEventListener('resize', resizeHandler)
+  }, [])
+
   return (
     <section className={style.main}>
       <GameCaption />
       <div className={style.wrapper}>
+        {clientWidth <= 768 ? <Blockquote /> : ''}
         <Garden />
-        <GameStart />
+        <GameStart clientWidth={clientWidth} />
       </div>
     </section>
   )
