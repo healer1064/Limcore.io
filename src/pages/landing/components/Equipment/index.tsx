d504ec9908d1e33e0123aa82b13610685f1b62af
dimaokk <dimadark216@gmail.com>
2022-02-07 21:57:33 +0700

    r

--- src/pages/landing/components/Equipment/index.tsx
+++ src/pages/landing/components/Equipment/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss'
 import Lottie from 'react-lottie'
 import map from '@animations/map.json'
 import useWindowSize from '@helpers/useWindowSizeHook'
-import img from '../../../../assets/images/eq_P_img.png'
+import img from '../../../../assets/images/p_img.png'
 
 export const Equipment: React.FC = () => {
   const { width } = useWindowSize()
@@ -20,11 +20,10 @@ export const Equipment: React.FC = () => {
       <div className={styles.container}>
         <h2 className={styles.title}>Где располагается оборудование</h2>
         <div className={styles.block}>
-          <div>
+          <div className={styles.desck}>
             <p>Мы приобретаем необходимое оборудование, располагая его децентрализованно в странах Евросоюза</p>
             <img src={img} alt='img' />
           </div>
-
           <Lottie options={defaultOptions} height={1024} width={809} />
         </div>
       </div>
--- src/pages/landing/components/Why/index.tsx
+++ src/pages/landing/components/Why/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.scss'
 import Lottie from 'react-lottie'
 import packmann from '@animations/packman.json'
 import useWindowSize from '@helpers/useWindowSizeHook'
-
+import noise from '../../../../assets/images/noise.png'
 export const Why: React.FC = () => {
   const { width } = useWindowSize()
   const defaultOptions = {
@@ -17,6 +17,7 @@ export const Why: React.FC = () => {
   return (
     <section className={styles.why}>
       <div className={styles.container}>
+        <div className={styles.noise} />
         <h2 className={styles.title}>Почему стоимость LIMC будет раст</h2>
         <p className={styles.description}>
           Экосистема Limcore не стоит на месте! Мы добавляем новые токены для майнинга и коины для валидации, увеличивая
