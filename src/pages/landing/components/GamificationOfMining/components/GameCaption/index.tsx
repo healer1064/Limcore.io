533c36a6b77c996848b193fc979a0463ddac62a4
Avenant <avenantmar@gmail.com>
2022-02-07 18:46:00 +0300

    add GameCaption block

---
+++ src/pages/landing/components/GamificationOfMining/components/GameCaption/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import style from './styles.module.scss'
+
+export const GameCaption: React.FC = () => {
+  return <h2 className={style.gameCaption}>Геймификация майнинга</h2>
+}
