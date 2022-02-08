bcb3bf901bed04cb72e8344d4a38ae267c9cd80f
Nikita Chernov <ns4ern@gmail.com>
2022-02-08 20:09:58 +0300

    hotfix bugs mobile version

--- src/pages/landing/components/Calcs/CalcsMobile/index.tsx
+++ src/pages/landing/components/Calcs/CalcsMobile/index.tsx
@@ -5,28 +5,44 @@ import { Modal } from '../Modal/Modal'
 import { dataMobile } from '../constants'
 import { BrownianMotion } from '../BrownianMotion/BrownianMotion'
 export const CalcsMobile = () => {
+  const [rangeValue, setRangeValue] = useState(1)
+  const [coin, setCoin] = useState('1')
   const [modal, setModal] = useState(false)
   const handleModal = () => {
     setModal(!modal)
+  }
+  const handleRange = (e, data) => {
+    const money = data * 0.002 * 24 * 30
+    setRangeValue(data)
+    if (String(Math.round(money)).length >= 4) {
+      setCoin(String(Math.round(money)).slice(0, 2) + 'K')
+    }
+    if (String(Math.round(money)).length < 5 && String(Math.round(money)).length > 3) {
+      setCoin(String(Math.round(money)).slice(0, 2).split('').join(',') + 'K')
+    }
+    if (String(Math.round(money)).length <= 3) {
+      setCoin(String(Math.round(money)).slice(0, 3))
+    }
   }
   return (
     <section className={styles.mobile}>
       <div className={styles.mobile__limc}>
         <div className={styles.mobile__logo} />
-        <label className={styles.mobile__counter}>10,000 LIMC</label>
+        <label className={styles.mobile__counter}>{rangeValue} LIMC</label>
         <div className={styles.mobile__div_txt}>
           <p className={styles.mobile__txt}>1 LIMC</p>
           <p className={styles.mobile__txt}>10,000 LIMC</p>
         </div>
         <StyledInputRange
           min={1}
-          max={10000}
+          max={40000}
           step={1}
           style={{ gridArea: '2/1', width: '335px', margin: '30px 0 16px 0', padding: '0' }}
+          onChange={handleRange}
         />
         <p className={styles.mobile__text}>Общий доход за 30d при текущем курсе</p>
         <div className={styles.mobile__coin_img} onClick={handleModal}>
-          <h3 className={styles.mobile__money}>$5</h3>
+          <h3 className={styles.mobile__money}>${coin}</h3>
           <span className={styles.mobile__info} onClick={handleModal} />
         </div>
         <div className={styles.mobile__borders}>
