e028c06a1196a3ece2b812fed8ed67882def0048
healer1064729 <healer1064729@gmail.com>
2022-02-08 18:22:08 +0300

    feature/header

--- src/pages/landing/components/Main/index.tsx
+++ src/pages/landing/components/Main/index.tsx
@@ -2,18 +2,23 @@ import React from 'react'
 import Styles from './styles.module.scss'
 
 import { MainCaption } from './components/MainCaption'
+import { MainCaptionMobile } from './components/MainCaptionMobile'
+import useWindowSize from '@helpers/useWindowSizeHook'
 // import { MainRounds } from './components/MainRounds'
-import { MainParthers } from './components/MainParthers'
-import { MainHow } from './components/MainHow'
+// import { MainParthers } from './components/MainParthers'
+// import { MainHow } from './components/MainHow'
 
 export const Main: React.FC = () => {
+  const { width } = useWindowSize()
+  const desktop = width >= 769
+
   return (
     <section className={Styles.main}>
       <div className={Styles.wrapper}>
-        <MainCaption />
+        {desktop ? <MainCaption /> : <MainCaptionMobile />}
         {/* <MainRounds /> */}
-        <MainParthers />
-        <MainHow />
+        {/* <MainParthers /> */}
+        {/* <MainHow /> */}
       </div>
     </section>
   )
