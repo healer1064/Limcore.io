ba1ff0be9a0b110c70d63da51cb098a4b34fb747
healer1064729 <healer1064729@gmail.com>
2022-02-10 12:01:01 +0300

    fix/header+main

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,41 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import Styles from './styles.module.scss'
 
 import { Main } from './components/Main'
 // import { Calculator } from './components/Calculator'
-import { GamificationOfMining } from './components/GamificationOfMining'
-import { AppForMining } from './components/AppForMining'
-import { RoadMap } from './components/RoadMap'
-import { Team } from './components/Team'
+// import { GamificationOfMining } from './components/GamificationOfMining'
+// import { AppForMining } from './components/AppForMining'
+// import { RoadMap } from './components/RoadMap'
+// import { Team } from './components/Team'
 // import { UpEcosystem } from './components/UpEcosystem'
 // import { OnLine } from './components/OnLine'
 // import { Questions } from './components/Questions'
 // import { Media } from './components/Media'
 // import { Footer } from '@components/Footer'
-import { Calcs } from '../landing/components/Calcs'
-import { CalcsMobile } from '../landing/components/Calcs/CalcsMobile'
-import useWindowSize from '@helpers/useWindowSizeHook'
+// import { Calcs } from '../landing/components/Calcs'
+// import { CalcsMobile } from '../landing/components/Calcs/CalcsMobile'
+// import useWindowSize from '@helpers/useWindowSizeHook'
 
 export const LandingPage: React.FC = () => {
-  const { innerWidth: width } = window
-
-  const [clientWidth, setClientWidth] = useState<number>(width)
-
-  useEffect(() => {
-    const resizeHandler = () => setClientWidth(window.innerWidth)
-    window.addEventListener('resize', resizeHandler)
-
-    return () => window.removeEventListener('resize', resizeHandler)
-  }, [])
+  // const { width } = useWindowSize()
 
   return (
     <div className={Styles.landing}>
       <Main />
       {/* <Calculator /> */}
-      <GamificationOfMining clientWidth={clientWidth} />
-      <AppForMining clientWidth={clientWidth} />
+      {/* <GamificationOfMining clientWidth={width} />
+      <AppForMining clientWidth={width} />
       <RoadMap />
-      <Team />
+      <Team /> */}
       {/* <UpEcosystem /> */}
       {/* <OnLine /> */}
       {/* <Questions />
