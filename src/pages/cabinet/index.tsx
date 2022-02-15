83885d023975a49e6d3c224c7af585aeb4c182cb
Avenant <avenantmar@gmail.com>
2022-02-15 21:05:53 +0300

    cabinet update

--- src/pages/cabinet/index.tsx
+++ src/pages/cabinet/index.tsx
@@ -6,15 +6,21 @@ import { ConverterLIMC } from './components/ConverterLIMC'
 import useWindowSize from '@helpers/useWindowSizeHook'
 import graphTemp from '@icons/GraphTemp.svg'
 import { AccordionUI } from './components/Accordion'
+import { ASide } from './components/ASide'
+import { Rating } from './components/ASide/Rating'
+import { NewCard } from './components/ASide/NewCard'
 
 export const CabinetPage: React.FC = () => {
   const { width } = useWindowSize()
+
+  const mobileCondition = width <= 768
 
   const graph = <img className={styles.cabinet__balanceGraph} src={graphTemp} />
 
   return (
     <div className={styles.cabinet}>
       <div className={styles.cabinet__container}>
+        {mobileCondition ? '' : <ASide clientWidth={width} />}
         <div>
           <section className={styles.cabinet__balance}>
             <h2>Баланс</h2>
@@ -23,10 +29,12 @@ export const CabinetPage: React.FC = () => {
                 <BalanceLIMC clientWidth={width} />
                 <ConverterLIMC />
               </div>
-              {width <= 940 ? '' : graph}
+              {width <= 1350 ? '' : graph}
             </div>
           </section>
-          {width <= 940 ? <AccordionUI text='Показать график доходности'>{graph}</AccordionUI> : ''}
+          {mobileCondition ? <Rating /> : ''}
+          {width <= 1350 ? <AccordionUI text='Показать график доходности'>{graph}</AccordionUI> : ''}
+          {mobileCondition ? <NewCard clientWidth={width} /> : ''}
         </div>
       </div>
     </div>
