a9244cc0ab70257355275c61751ac2478e577ea2
healer1064729 <healer1064729@gmail.com>
2022-02-19 10:55:10 +0300

    fix/footer

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -22,7 +22,7 @@ import { FooterMobile } from '@components/Footer/FooterMobile'
 // import { Footer } from '@components/Footer'
 import { Calcs } from '../landing/components/Calcs'
 import { CalcsMobile } from '../landing/components/Calcs/CalcsMobile'
-import { Orbit } from '../landing/components/Orbit/'
+// import { Orbit } from '../landing/components/Orbit/'
 
 export const LandingPage: React.FC = () => {
   const { width } = useWindowSize()
@@ -39,7 +39,7 @@ export const LandingPage: React.FC = () => {
       <GamificationOfMining clientWidth={width} />
       <AppForMining clientWidth={width} />
       <RoadMap />
-      <Orbit />
+      {/* <Orbit /> */}
       {/* <Team /> */}
       {/* <UpEcosystem /> */}
       {/* <OnLine /> */}
